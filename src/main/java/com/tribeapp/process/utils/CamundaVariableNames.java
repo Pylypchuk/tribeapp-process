dd7e744348f5315da819452aa18434e00965ffc7
Alex Pylypchuk <oleksii.pylypchuk@sixt.com>
2020-07-30 09:56:51 +0300

    development julian changes

---
+++ src/main/java/com/tribeapp/process/utils/CamundaVariableNames.java
@@ -0,0 +1,7 @@
+package com.tribeapp.process.utils;
+
+public final class CamundaVariableNames {
+    private CamundaVariableNames() {}
+
+    public static final String ASSET_SPECIFICATION = "Asset specification";
+}
